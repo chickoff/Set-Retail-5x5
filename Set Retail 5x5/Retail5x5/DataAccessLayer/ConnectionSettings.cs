c88b18a891121820f0571b37f666c7fd2fe024be
chikov_aa <chickoff@gmail.com>
2021-03-25 09:56:49 +0300

    pwd fix

--- Set Retail 5x5/Retail5x5/DataAccessLayer/ConnectionSettings.cs
+++ Set Retail 5x5/Retail5x5/DataAccessLayer/ConnectionSettings.cs
@@ -9,9 +9,9 @@ namespace Set_Retail_5x5.Retail5x5.DataAccessLayer
     public static class ConnectionSettings
     {
         public static string ConMsStr { get; } =
-            "Server=192.168.5.9;User Id=sa;Password=cgjhnkjnj80$;Database=AdvancedServices;";
+            "";
 
         public static string ConPgStrDbSet { get; } =
-            "Server=192.168.5.36;Port=5432;User Id=postgres;Password=postgres;Database=set;";
+            "";
     }
 }
